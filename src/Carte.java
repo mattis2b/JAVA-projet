b2f1145d1dfe2e46915955a3e99593c04f260830
axel <axel@localhost>
2021-03-18 19:08:13 +0100

    Classe "carte" avec le code en rapport avec la carte du donjon
    Classe "Joueur" avec le code en rapport avec les infos sur le joueur
    
    Classe "JoueurEtCarte" avec le code intégrale
    
    Vous pouvez supp le reste

---
+++ src/Carte.java
@@ -0,0 +1,37 @@
+
+public class Carte {
+
+	public static void main(String[] args) {
+
+		private int i = 15;          
+		private int j = 15;          
+		private String[][]mapObjet = new String[15][15]; //creation de la map de largeur et hauteur 15
+		
+		public Carte(int a) {                   //fonction de creation de map
+			for(int i = 0; i< 15; i++) {         //boucle parcourant l'axe des x
+				for(int j = 0; i< 15; i++) {     //boucle parcourant l'axe des y
+					mapObjet[i][j]= ",";         //action pour mettre une virgule (signe comme quoi la case est vide) 
+					                             //sur chaque case
+				} 
+
+			}
+	}
+		for(int i = 0; i< 4; i++) {                //boucle qui place les soins (s), pièges (p) et murs (!)  
+			for(int j = 0; j< 4; j++) {
+				if(mapObjet[i][j].equals(",")) {
+					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "s"; 
+				}
+				if(mapObjet[i][j].equals(",")) {
+					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "p"; 
+				}
+				if(mapObjet[i][j].equals(",")) {
+					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "!"; 
+				}
+			}
+
+		}
+		
+		
+
+}
+}
---
+++ src/Joueur.java
@@ -0,0 +1,68 @@
+
+public class Joueur {
+
+	public static void main(String[] args) {  //à demander au prof, cette methode main fait apparaitre des erreurs 
+		
+		int Pv = 10;        //creation des points de vie
+		int Inventaire;    //creation de l'inventaire vide 
+		
+		public int getting() {         //r�cup�ration et affichage des pv du joueur
+			return Pv;
+		}
+		
+		public void setting(int Pv) {
+			if(Pv >0) {
+				this.Pv=Pv;
+				System.err.println("Vous avez"+Pv+"points de vie.");
+			}
+			else {
+				System.err.println("Vous avez perdu la partie.");	
+			}
+				
+		}
+		
+		public int getInventaire() {        //r�cup�ration et affichage de l'inventaire du joueur
+			return Inventaire;
+		}
+		
+		public void ramasser() {            //fonction ramassage d'un objet 
+			if(Inventaire < 5 && mapObjet [i] [j].equals("p")) {
+				Inventaire += 1;                   // ajout de l'objet,dans l'inventaire si celui ci n'est pas plein
+				for(int i = 0; i<1; i++) {         // boucle de rajout d'un soin sur la map
+					for(int j = 0; j<1; j++) {     // si il y a eu ajout dans l'inventaire
+						boolean b = true;
+						while(b) {
+							if(mapObjet[i][j].equals(",") && b) {
+								mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "s";
+								b = false;
+							}
+						}
+					}
+				}
+				mapObjet[i][j] = ",";
+			}
+			else {                           // si l'inventaire est plein, on affiche la phrase au joueur 
+				if(Inventaire >= 5) {
+					System.err.println("L'invenatire est déjà plein");
+				}
+				else {                       // si le joueur ramasse et qu'il n'y a pas de soins on affiche la phrase 
+					System.err.println("Pas d'objet à ramasser");
+				}
+			}
+		}
+		
+		
+		
+		
+	}
+	
+		
+		
+		
+		
+		
+	}
+	
+
+	
+	
