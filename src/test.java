f321a54e95eec3e5c72beb83bb66476053f1dfbd
axel <axel@localhost>
2021-03-18 19:13:50 +0100

    Classe "carte" avec le code en rapport avec la carte du donjon
    Classe "Joueur" avec le code en rapport avec les infos sur le joueur
    
    Classe "JoueurEtCarte" avec le code intégrale

--- src/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-
-//Les deux classes tests étaient là pour regler le pb avec Git.
-	// A NE PLUS MODIFIER
-	// A NE PLUS MODIFIER
-	// A NE PLUS MODIFIER
-	
-}
--- src/test2.java
+++
@@ -1,5 +0,0 @@
-
-public class test2 {
-
-}
-//Est-ce que ça marche
