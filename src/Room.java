70ffe2235b41756922625411b63c9b3da70751e5
33643 <33643@LAPTOP-O4184V7L>
2021-03-19 17:59:36 +0100

    la méthode main ne renvoie rien

---
+++ src/Room.java
@@ -0,0 +1,14 @@
+
+public class Room {
+
+	public static void main(String[] args) {
+        Carte carteTest1 = new Carte(15,15,"x");
+        
+        
+        
+        System.out.println	(carteTest1);
+
+      
+
+}
+}
