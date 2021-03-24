85cb11da744ce4330cadddda823a7fba3c8e3200
33643 <33643@LAPTOP-O4184V7L>
2021-03-24 16:38:32 +0100

    Mise en place sockets (début)

---
+++ src/TestServeurTCP.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+public class TestServeurTCP {
+  final static int port = 9792;
+
+  public static void main(String[] args) {
+    try {
+      ServerSocket socketServeur = new ServerSocket(port);
+      System.out.println("Bonjour, bienvenue sur le serveur Donjon");
+
+      while (true) {
+        Socket socketClient = socketServeur.accept();
+        String message = "";
+
+        System.out.println("Connexion avec : "+socketClient.getInetAddress());
+
+        // InputStream in = socketClient.getInputStream();
+        // OutputStream out = socketClient.getOutputStream();
+
+        BufferedReader in = new BufferedReader(
+          new InputStreamReader(socketClient.getInputStream()));
+        PrintStream out = new PrintStream(socketClient.getOutputStream());
+        message = in.readLine();
+        out.println(message);
+
+        socketClient.close();
+        socketServeur.close();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
