c313610d1cc410503f5b6df6d70d310eed97457c
Stathis Delivorias <jazzcrazysta@yahoo.com>
2021-04-09 19:33:53 +0200

    commiting and pushing

--- src/Carte.java
+++ src/Carte.java
@@ -3,42 +3,57 @@ public class Carte { //
 	          
 	String[][]mapObjet = new String[15][15]; //creation de la map de largeur et hauteur 15
 	String[][]pointdevueJoueur = new String[15][15]; //creation de la map de largeur et hauteur 15
-
-		int i;  
-		int j; 
+	int iVictoire=(int) (Math.random()*(15));
+	int jVictoire=(int) (Math.random()*(15));
 		
 		
 		Carte() {                   //fonction de creation de map
 			for(int i = 0; i< 15; i++) {         //boucle parcourant l'axe des x
-				for(int j = 0; j< 15; j++) {     //boucle parcourant l'axe des y
-					mapObjet[i][j]= "-";         //action pour mettre une virgule (signe comme quoi la case est vide) 
+				for(int j = 0; j< 15; j++) { //boucle parcourant l'axe des y
+					int symbole = (int)(Math.random()*(5));
+					if (symbole==1){
+						mapObjet[i][j]= "-";
+					}
+					else if (symbole==2){
+						mapObjet[i][j]= "s";
+					}
+					else if (symbole==3){
+						mapObjet[i][j]= "p";
+					}
+					else if (symbole==4){
+						mapObjet[i][j]= "!";
+					}
+					else if (symbole==5){
+						mapObjet[i][j]= "M";
+					}
+					         //action pour mettre une virgule (signe comme quoi la case est vide) 
 					                             //sur chaque case 
 				} 
 
 			}
 			
-			for(int i = 0; i< 6; i++) {     //boucle qui place les soins (s), pièges (p) et murs (!)  
-			for(int j = 0; j< 6; j++) {
-				if(mapObjet[i][j].equals("-")) {
-					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "s"; 
-				}
-				if(mapObjet[i][j].equals("-")) {
-					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "p"; 
-				}
-				if(mapObjet[i][j].equals("-")) {
-					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "!"; 
-				}
-				if(mapObjet[i][j].equals("-")) {
-					mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "M"; 
-				}
+			mapObjet[iVictoire][jVictoire]= "V"; 
+		
+			//for(int i = 0; i< 6; i++) {     //boucle qui place les soins (s), pièges (p) et murs (!)  
+			//for(int j = 0; j< 6; j++) {
+			//	if(mapObjet[i][j].equals("-")) {
+			//		mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "s"; 
+			//	}
+			//	if(mapObjet[i][j].equals("-")) {
+			//		mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "p"; 
+			//	}
+			//	if(mapObjet[i][j].equals("-")) {
+			//		mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "!"; 
+			//	}
+			//	if(mapObjet[i][j].equals("-")) {
+			//		mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "M"; 
+			//	}
 				
-			}
+			//}
 		}
-			if(mapObjet[i][j].equals("-")) {
-				mapObjet[(int) (Math.random()*(15))][(int) (Math.random()*(15))]= "V"; 
-			}
 			
-		}
+		
+			
 		public void afficher () {
 			System.out.println();
 			for(int i = 0; i< 15; i++) {        
